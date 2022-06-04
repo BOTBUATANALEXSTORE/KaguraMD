dc5c42b4eeb7e561125ccb43e5ef642f18c151f8
BOTBUATANALEXSTORE <106811197+BOTBUATANALEXSTORE@users.noreply.github.com>
2022-06-05 06:03:50 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,12 +10,12 @@ global.APIKeys = {
 }
 
 //---------( SETTING )---------//
-global.owner = ['6282279915237','6285789004732','6285724706876']
-global.premium = ['6282279915237']
-global.ownernomer = '6282279915237'
-global.ownername = 'Lexxy Official'
-global.botname = 'KaguraMD'
-global.footer = 'Lexxy Official'
+global.owner = ['6285803459211']
+global.premium = ['6285803459211']
+global.ownernomer = '6285803459211'
+global.ownername = 'Sup bot'
+global.botname = 'sup bot'
+global.footer = 'sup bot'
 global.thumb = fs.readFileSync('./image/fake.jpg')
 global.veloriy = fs.readFileSync('./image/kagura.mp4')
 global.sessionName = 'session'
